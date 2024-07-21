8b6ff06711b192f4ee5124e3ae40f4ac019c3b6e
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-07-22 00:07:52 +0900

    chore: relocate components into components folder

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,8 +1,10 @@
+import { MyButton } from "@tripie/design-system";
+
 export default function Home() {
   return (
     <div>
       {/* this is home */}
-      {/* <MyButton>Click MyButton!</MyButton> */}
+      <MyButton>Click MyButton!</MyButton>
     </div>
   );
 }
