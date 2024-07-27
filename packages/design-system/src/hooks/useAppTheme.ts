a9842168867891fb1e83da11828406f9df9dfb0a
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-07-27 16:52:46 +0900

    refactor: setTheme only by setThemeMode

--- packages/design-system/src/hooks/useAppTheme.ts
+++ packages/design-system/src/hooks/useAppTheme.ts
@@ -6,57 +6,41 @@ const COLOR_SCHEME_QUERY = "(prefers-color-scheme: dark)";
 const THEME_MODE = {
   DARK: "dark",
   LIGHT: "light",
-} as const;
-
-const CONTROL_MODE = {
-  USER: "user",
-  OS_DEFAULT: "os_default",
+  OS_DEFAULT: null,
 } as const;
 
 type ThemeMode = (typeof THEME_MODE)[keyof typeof THEME_MODE];
-type ControlMode = (typeof CONTROL_MODE)[keyof typeof CONTROL_MODE];
 
 type UseAppThemeOutput = {
   mode: ThemeMode;
   toggle: () => void;
-  setLight: () => void;
-  setDark: () => void;
   setMode: (mode: ThemeMode) => void;
-  setControl: (control: ControlMode) => void;
 };
 
 export const useAppTheme = (): UseAppThemeOutput => {
   const isDarkOS = useMediaQuery(COLOR_SCHEME_QUERY);
-  const osPrefersMode = isDarkOS ? THEME_MODE.DARK : THEME_MODE.LIGHT;
   const [themeMode, setThemeMode] = useLocalStorage<ThemeMode>(
     "app-theme",
-    osPrefersMode
-  );
-  const [themeControl, setThemeControl] = useLocalStorage<ControlMode>(
-    "control-theme",
-    CONTROL_MODE.OS_DEFAULT
+    THEME_MODE.OS_DEFAULT
   );
 
   useEffect(() => {
     const root = document?.documentElement;
-
-    if (themeControl === "os_default") {
-      setThemeMode(isDarkOS ? THEME_MODE.DARK : THEME_MODE.LIGHT);
+    if (themeMode == null) {
+      console.log(themeMode, isDarkOS);
+      root.dataset.theme = isDarkOS ? THEME_MODE.DARK : THEME_MODE.LIGHT;
+    } else {
+      root.dataset.theme = themeMode;
     }
-    root.dataset.theme = themeMode;
-  }, [isDarkOS, osPrefersMode, themeControl, themeMode]);
+  }, [themeMode, isDarkOS]);
 
   return {
     mode: themeMode,
     toggle: () => {
-      setThemeControl("user");
       setThemeMode((previous) =>
         previous === THEME_MODE.DARK ? THEME_MODE.LIGHT : THEME_MODE.DARK
       );
     },
-    setLight: () => setThemeMode(THEME_MODE.LIGHT),
-    setDark: () => setThemeMode(THEME_MODE.DARK),
     setMode: (mode) => setThemeMode(mode),
-    setControl: (control) => setThemeControl(control),
   };
 };
