73b90d9c878d93bda7efa1c955ced1de9ec31599
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-07-31 20:41:30 +0900

    chore: add todo

--- apps/web/components/__test__/themeButton.test.tsx
+++ apps/web/components/__test__/themeButton.test.tsx
@@ -4,34 +4,16 @@ import { cleanup, render } from "@testing-library/react";
 // import ThemeButton from "../ThemeButton";
 import "@testing-library/jest-dom";
 import "@testing-library/jest-dom/jest-globals";
-import { Text } from "@tripie/design-system";
+import ThemeButton from "../ThemeButton/ThemeButton";
 // import ThemeButton from "../ThemeButton/ThemeButton";
 // jest.mock("next/font/local");
 
 afterEach(cleanup);
+//!! TODO : [FIX] TypeError: Cannot read properties of null (reading 'useState')
 
-// it("text has 'os control'", () => {
-//   const { getByText } = render(<Text>os control</Text>);
-//   expect(getByText("os control"));
-// });
-
-// describe("button", () => {
-//   test("render test", () => {
-//     const { getByText } = render(<ThemeButton />);
-//     expect(getByText("os control")).not.toThrow();
-//   });
-// });
-
-it("text has 'os control'", () => {
-  //   // localFont({
-  //   //   src: [
-  //   //     {
-  //   //       path: "../../../static/fonts/MaruBuri-Regular.woff",
-  //   //       style: "normal",
-  //   //     },
-  //   //   ],
-  //   // });
-  const { getByText } = render(<Text>os control</Text>);
-
-  expect(getByText("os control")).toBeInTheDocument();
+describe("button", () => {
+  test("has text 'os control'", () => {
+    const { queryByText } = render(<ThemeButton />);
+    expect(queryByText("os control")).toReturn();
+  });
 });
