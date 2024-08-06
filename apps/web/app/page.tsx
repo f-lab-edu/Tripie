ddedde450384917117f793de8f8fc0901410cec6
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-08-06 19:49:02 +0900

    feat: move display setting to center or right class

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { Headers, Text } from "@tripie/design-system";
-import Layout from "@tripie/design-system/components/layout/_layout";
+import Container from "@tripie/design-system/components/container/_container";
 import UnstyledLink from "@tripie/design-system/components/typography/link/_link";
 import Paragraph from "@tripie/design-system/components/typography/paragraph/_paragraph";
 import ThemeButton from "../components/ThemeButton";
@@ -9,28 +9,27 @@ export default function Home() {
   const text =
     "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis\npariatur, ab voluptates saepe eum at excepturi, eaque accusamus labore\ntemporibus ex nostrum in hic iure porro quod doloribus deleniti! Qui.\ntemporibus ex nostrum in hic iure porro quod doloribus deleniti!";
   return (
-    <div>
+    <Container margin="sm">
       <ThemeButton />
       <ThemeButton.Toggle />
 
       <Headers>H1 emphasized</Headers>
       <Headers.H1>H1 with headlines</Headers.H1>
-      <Layout>
-        <Text>Wrapped by Layout - default</Text>
-      </Layout>
-      <Layout.Center>
-        <Text>Wrapped by Layout - center</Text>
-      </Layout.Center>
-      <Layout.Right>
-        <Text>Wrapped by Layout - right</Text>
-      </Layout.Right>
+
+      <Text>Wrapped by Layout - default</Text>
+
+      <Text>Wrapped by Layout - center</Text>
+
+      <Text>Wrapped by Layout - right</Text>
 
       <Headers.H2>H2</Headers.H2>
       <Headers.H3>H3</Headers.H3>
       <Headers.H4>H4</Headers.H4>
-      <Text>{text}</Text>
-      <Paragraph>{text}</Paragraph>
+      <Container align="center">
+        <Text>{text}</Text>
+        <Paragraph>{text}</Paragraph>
+      </Container>
       <UnstyledLink href="/">떡볶이</UnstyledLink>
-    </div>
+    </Container>
   );
 }
--- packages/design-system/src/components/layout/_layout.tsx
+++ packages/design-system/src/components/layout/_layout.tsx
@@ -24,7 +24,7 @@ const Layout = ({
       ])}
       {...props}
     >
-      {children}
+      <div>{children}</div>
     </div>
   );
 };
