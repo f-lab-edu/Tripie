6373f314a8b163f393ac7aa60111585bd68572fe
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-08-30 16:01:16 +0900

    fix: nav opacity not disabling content

--- apps/web/components/landing/nav/Menu/Navigation.tsx
+++ apps/web/components/landing/nav/Menu/Navigation.tsx
@@ -9,10 +9,12 @@ const cx = classNames.bind(Style);
 const variants = {
   open: {
     opacity: 1,
+    display: 'inline-block',
     transition: { staggerChildren: 0.07, delayChildren: 0.2 },
   },
   closed: {
     opacity: 0,
+    display: 'none',
     transition: { staggerChildren: 0.05, staggerDirection: -1 },
   },
 };
