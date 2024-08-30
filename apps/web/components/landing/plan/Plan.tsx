f8751be4331a5643605ba57967cc491c3e2d823b
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-08-30 12:44:22 +0900

    feat: add plan temporary component

---
+++ apps/web/components/landing/plan/Plan.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+import classNames from 'classnames/bind';
+import Style from './plan.module.scss';
+const cx = classNames.bind(Style);
+
+export default function Plan() {
+  return <section className={cx('plan')}>plans</section>;
+}
