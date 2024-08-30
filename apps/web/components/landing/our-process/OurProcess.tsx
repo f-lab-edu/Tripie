b90c19538d991760de5f98c3c935da7f46418bc1
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-08-30 14:24:05 +0900

    feat: add link id to each menu item

--- apps/web/components/landing/our-process/OurProcess.tsx
+++ apps/web/components/landing/our-process/OurProcess.tsx
@@ -5,5 +5,9 @@ import Style from './our-process.module.scss';
 const cx = classNames.bind(Style);
 
 export default function OurProcess() {
-  return <section className={cx('our-process')}>our process</section>;
+  return (
+    <section className={cx('our-process')} id="Process">
+      our process
+    </section>
+  );
 }
