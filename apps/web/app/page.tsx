6eccf7e17a8d88cbd21d40ae4a178f73dc4a80fe
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-09-02 12:47:53 +0900

    feat: set landing page route as /

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,14 +1,32 @@
 'use client';
-import { Button } from '@tripie/design-system';
-import ThemeButton from 'components/ThemeButton';
-import { useRouter } from 'next/navigation';
+import AboutUs from 'components/landing/about-us/AboutUs';
+import Contact from 'components/landing/contact/Contact';
+import Faq from 'components/landing/faq/Faq';
+import Footer from 'components/landing/footer/Footer';
+import Header from 'components/landing/header/Header';
+import Loading from 'components/landing/loading/loading';
+import OurProcess from 'components/landing/our-process/OurProcess';
+import OurService from 'components/landing/our-service/OurService';
+import OurWork from 'components/landing/our-work/OurWork';
+import Plan from 'components/landing/plan/Plan';
+import Team from 'components/landing/team/Team';
+import Testimonials from 'components/landing/testimonials/Testimonials';
 
 export default function Home() {
-  const router = useRouter();
   return (
     <>
-      <Button onClick={() => router.push('/playground/landing')}>to landing playground</Button>
-      <ThemeButton.Toggle />
+      <Loading />
+      <Header />
+      <AboutUs />
+      <OurProcess />
+      <OurService />
+      <OurWork />
+      <Plan />
+      <Testimonials />
+      <Team />
+      <Contact />
+      <Faq />
+      <Footer />
     </>
   );
 }
