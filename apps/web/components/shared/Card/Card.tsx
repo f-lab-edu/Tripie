ba5f49c97776f958ee215c4516e11fee4fac9a3b
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-09-03 18:49:35 +0900

    feat: common, card component

---
+++ apps/web/components/shared/Card/Card.tsx
@@ -0,0 +1,57 @@
+import classNames from 'classnames/bind';
+
+import { Container } from '@tripie/design-system';
+import { ReactNode } from 'react';
+import MotionSlideUp from '../MotionSlideUp/MotionSlideUp';
+
+import { MotionSlideUpProps } from 'types/Animation';
+import Style from './card.module.scss';
+
+const cx = classNames.bind(Style);
+
+const Card = ({ duration, replays, delay = 0.3, children, className }: MotionSlideUpProps) => {
+  return (
+    <MotionSlideUp
+      delay={delay}
+      duration={duration}
+      replays={replays}
+      className={cx('with-border', 'outer-wrap', className)}
+    >
+      {children}
+      <div className={cx('noise')}></div>
+    </MotionSlideUp>
+  );
+};
+
+const CardInnerWrap = ({
+  children,
+  className,
+}: Readonly<{
+  children: ReactNode;
+  className?: string;
+}>) => {
+  return (
+    <Container margin="none" className={cx('inner-wrap', className)}>
+      {children}
+    </Container>
+  );
+};
+
+const CardDescription = ({
+  children,
+  className,
+}: Readonly<{
+  children: ReactNode;
+  className?: string;
+}>) => {
+  return (
+    <Container margin="none" className={cx('inner-wrap', 'with-description', className)}>
+      {children}
+    </Container>
+  );
+};
+
+Card.Content = CardInnerWrap;
+Card.Description = CardDescription;
+
+export default Card;
