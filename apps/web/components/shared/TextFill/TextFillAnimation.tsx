722e4847a3ba003b59c9782c8bffc4b4eaa28d7f
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-09-03 18:50:20 +0900

    feat: text fill animation component with react-intersection-observer

--- apps/web/components/shared/TextFill/TextFillAnimation.tsx
+++ apps/web/components/shared/TextFill/TextFillAnimation.tsx
@@ -1,12 +1,54 @@
 'use client';
 import classNames from 'classnames/bind';
-import { motion } from 'framer-motion';
+import COLORS from 'constants/colors';
+import { MotionStyle, motion } from 'framer-motion';
+
 import { ReactNode } from 'react';
+import { InView } from 'react-intersection-observer';
+import { AnimationProps } from 'types/Animation';
 import Style from './text-fill-animation.module.scss';
 
 const cx = classNames.bind(Style);
 
 const TextFillAnimation = ({
+  text,
+  startColor = COLORS[800],
+  endColor = COLORS[0],
+  duration = 1,
+  delay = 0,
+  replays = true,
+}: Partial<AnimationProps>) => {
+  return (
+    <InView>
+      {({ inView, ref }) => (
+        <div className={cx('animation')} ref={ref}>
+          <span className={cx('wrap')}>
+            {text}
+            <motion.span
+              className={cx('text')}
+              style={
+                {
+                  color: endColor,
+                } as MotionStyle
+              }
+              animate={inView ? 'visible' : 'hidden'}
+              initial={{ width: '0%' }}
+              variants={{
+                visible: { width: '100%' },
+                hidden: { width: '0%' },
+              }}
+              transition={{ startColor, endColor, duration, delay, replays }}
+            >
+              {text}
+            </motion.span>
+          </span>
+        </div>
+      )}
+    </InView>
+  );
+};
+
+const TitleTextFillAnimation = ({
   children,
 }: Readonly<{
   children: ReactNode;
