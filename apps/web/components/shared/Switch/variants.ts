8b736efc4c61b0c72eb254716e5ed658933d2893
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-09-03 18:51:28 +0900

    feat: common, switch component

---
+++ apps/web/components/shared/Switch/variants.ts
@@ -0,0 +1,34 @@
+import COLORS from 'constants/colors';
+import { Variants } from 'framer-motion';
+
+export const SWITCH_VARIANTS = {
+  BACKGROUND: {
+    off: () => ({
+      backgroundColor: COLORS[40],
+      borderColor: COLORS[400],
+    }),
+    on: () => ({
+      backgroundColor: COLORS[50],
+      borderColor: COLORS[50],
+    }),
+  } as Variants,
+  BUTTON: {
+    off: () => ({
+      x: '150%',
+      backgroundColor: COLORS[40],
+    }),
+    on: () => ({
+      x: '30%',
+      backgroundColor: COLORS[50],
+    }),
+  } as Variants,
+  TEXT: {
+    off: () => ({
+      x: '0%',
+    }),
+    on: () => ({
+      x: '250%',
+      color: COLORS[50],
+    }),
+  } as Variants,
+};
