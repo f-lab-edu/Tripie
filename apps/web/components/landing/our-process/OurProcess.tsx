2590529b83a4f47781a26c3be3b769a93b758052
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-09-04 13:35:13 +0900

    refactor: our-process, remove duplicate code

--- apps/web/components/landing/our-process/OurProcess.tsx
+++ apps/web/components/landing/our-process/OurProcess.tsx
@@ -8,6 +8,39 @@ import Subscription from './subscription/Subscription';
 
 const cx = classNames.bind(Style);
 
+const cards = [
+  {
+    label: 'Subscribe',
+    content: <Subscription />,
+    description:
+      "Choose your preferred plan to start and cancel or pause at anytime you like. So you're as flexible as your business' needs.",
+  },
+  {
+    label: 'Request',
+    content: 'content',
+    description:
+      'Start requesting the workflow-automations and AI applications you need, your developers are right there to transform your ideas into reality.',
+  },
+  {
+    label: 'Build',
+    content: 'content',
+    description:
+      'Our developers swiftly begin building your custom solutions, prioritising speed without compromising on quality.',
+  },
+  {
+    label: 'Test & optimise',
+    content: 'content',
+    description:
+      "You either approve or request revisions - we're dedicated to refining our builds until you're fully satisfied.",
+  },
+  {
+    label: 'Become an industry leader',
+    content: 'content',
+    description:
+      ' Continue requesting as many workflow-automations and AI-applications as you wish, and transform your organisation into a worldwide industry leader.',
+  },
+];
+
 export default function OurProcess() {
   return (
     <section className={cx('our-process')} id="Process">
@@ -18,53 +51,14 @@ export default function OurProcess() {
           </Headings.H2>
         </MotionSlideUp>
         <Container className={cx('wrap')} margin="l" applyMargin="top-bottom">
-          <Card>
-            <Card.Content>
-              <Subscription />
-            </Card.Content>
-            <Card.Description>
-              <Description descriptionTitle="01. Subscribe">
-                Choose your preferred plan to start and cancel or pause at anytime you like. So you're as flexible as
-                your business' needs.
-              </Description>
-            </Card.Description>
-          </Card>
-          <Card>
-            <Card.Content>// !! !! TODO : replace with '02. Request' 02. Request</Card.Content>
-            <Card.Description>
-              <Description descriptionTitle="02. Request">
-                Start requesting the workflow-automations and AI applications you need, your developers are right there
-                to transform your ideas into reality.
-              </Description>
-            </Card.Description>
-          </Card>
-          <Card>
-            <Card.Content>// !! !! TODO : replace with '03. Build' 03. Build</Card.Content>
-            <Card.Description>
-              <Description descriptionTitle="03. Build">
-                Our developers swiftly begin building your custom solutions, prioritising speed without compromising on
-                quality.
-              </Description>
-            </Card.Description>
-          </Card>
-          <Card>
-            <Card.Content>// !! !! TODO : replace with '04. Test & optimise' 04. Test & optimise</Card.Content>
-            <Card.Description>
-              <Description descriptionTitle="04. Test & optimise">
-                You either approve or request revisions - we're dedicated to refining our builds until you're fully
-                satisfied.
-              </Description>
-            </Card.Description>
-          </Card>
-          <Card>
-            <Card.Content>// !! !! TODO : replace with '05. Become an industry leader'</Card.Content>
-            <Card.Description>
-              <Description descriptionTitle="05. Become an industry leader">
-                Continue requesting as many workflow-automations and AI-applications as you wish, and transform your
-                organisation into a worldwide industry leader.
-              </Description>
-            </Card.Description>
-          </Card>
+          {cards.map(({ label, content, description }, index) => (
+            <Card key={label}>
+              <Card.Content>{content}</Card.Content>
+              <Card.Description>
+                <Description descriptionTitle={`${index + 1}. ${label}`}>{description}</Description>
+              </Card.Description>
+            </Card>
+          ))}
         </Container>
       </Container>
     </section>
