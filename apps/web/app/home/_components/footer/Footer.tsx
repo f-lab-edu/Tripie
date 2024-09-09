c0b2cef434c627dd02eec5ec6c82fc50d6a98bdd
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-09-09 16:29:51 +0900

    feat: add home/footer

---
+++ apps/web/app/home/_components/footer/Footer.tsx
@@ -0,0 +1,39 @@
+'use client';
+import { Container } from '@tripie/design-system';
+import classNames from 'classnames/bind';
+import ROUTES from 'constants/routes';
+import { useRouter } from 'next/navigation';
+import Divider from 'shared/components/Divider/Divider';
+import List from 'shared/components/List/List';
+import TextUnderLineAnimation from 'shared/components/TextUnderlineAnimation/TextUnderlineAnimation';
+import Style from './footer.module.scss';
+
+const cx = classNames.bind(Style);
+
+export default function Footer() {
+  const router = useRouter();
+  return (
+    <footer className={cx('footer')}>
+      <Container margin="m" applyMargin="left-right">
+        <Container margin="none" className={cx('wrap')}>
+          <TextUnderLineAnimation>
+            mail<span className={cx('accented')}>@</span>tripie.com
+          </TextUnderLineAnimation>
+          <Divider />
+        </Container>
+        <List>
+          <li>© 2024</li>
+          <li>mail@tripie.com</li>
+        </List>
+        <Divider />
+        <List>
+          {ROUTES.PAGE.LANDING.map(page => (
+            <li onClick={() => router.push(page.href)} key={page.label}>
+              <TextUnderLineAnimation className={cx('list-item')}>{page.label}</TextUnderLineAnimation>
+            </li>
+          ))}
+        </List>
+      </Container>
+    </footer>
+  );
+}
