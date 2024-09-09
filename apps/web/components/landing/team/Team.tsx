6c750f48ad08e0f16166832d4f2f0e76f21b43b4
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-09-09 13:58:04 +0900

    chore: remove unused sections

--- apps/web/components/landing/team/Team.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-import classNames from 'classnames/bind';
-import Style from './team.module.scss';
-const cx = classNames.bind(Style);
-
-export default function Team() {
-  return <section className={cx('team')}>our team</section>;
-}
