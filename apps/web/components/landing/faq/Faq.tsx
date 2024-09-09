451378b9ad72f7c7fc0e11e0aceaa67438678dcd
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-09-09 14:49:59 +0900

    chore: remove relocated files

--- apps/web/components/landing/faq/Faq.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import classNames from 'classnames/bind';
-import Style from './faq.module.scss';
-const cx = classNames.bind(Style);
-
-export default function Faq() {
-  return <section className={cx('faq', 'section')}>faq</section>;
-}
