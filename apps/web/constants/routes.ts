b0e24e288f254fe679dce6205e08c9130c8285a6
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-09-09 16:54:42 +0900

    feat: add routes

--- apps/web/constants/routes.ts
+++ apps/web/constants/routes.ts
@@ -1,17 +1,22 @@
-type Routes = { label: string; href: string };
-
 const ROUTES = {
   API: {},
   PAGE: {
     LANDING: [
       { label: 'Process', href: '#Process' },
       { label: 'Services', href: '#Services' },
-      ,
       { label: 'Work', href: '#Work' },
       { label: 'Plans', href: '#Plans' },
       { label: 'Contact', href: '#Contact' },
-    ] as Routes[],
+    ],
     PLAYGROUND: { label: 'Playground', href: '/playground' },
+    HOME: { label: 'Home', href: '/' },
+    SERVICES: { label: 'Services', href: '/#Services' },
+    CONTACT: { label: 'Contact', href: '/#Contact' },
+  },
+  RESOURCE: {
+    ARROW: { label: 'icon', src: '/arrow.png' },
+    CHECK: { label: 'icon', src: '/check.png' },
+    X: { label: 'icon', src: '/x.png' },
   },
 };
 
