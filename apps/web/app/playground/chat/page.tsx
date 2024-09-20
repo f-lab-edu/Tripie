3203577b90011021b0c0b79742f9a2e633dac69a
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-09-20 18:50:46 +0900

    chore: remove stringified funnel.historySteps

--- apps/web/app/playground/chat/page.tsx
+++ apps/web/app/playground/chat/page.tsx
@@ -16,32 +16,29 @@ const Chat = () => {
     },
   });
   return (
-    <>
-      {JSON.stringify(funnel.historySteps)}
-      <funnel.Render
-        A={({ context, history }) => (
-          <div>
-            <h1>A Step</h1>
-            <h3>{JSON.stringify(context)}</h3>
-            <button onClick={() => history.push('B', { a: 'a' })}>Next</button>
-          </div>
-        )}
-        B={({ context, history }) => (
-          <div>
-            <h1>B Step</h1>
-            <p>a: {context.a}</p>
-            <BFunnel a={context.a} onNext={b => history.push('C', { b })} />
-          </div>
-        )}
-        C={({ context }) => (
-          <div>
-            <h1>C Step</h1>
-            <p>a: {context.a}</p>
-            <p>b: {context.b}</p>
-          </div>
-        )}
-      />
-    </>
+    <funnel.Render
+      A={({ context, history }) => (
+        <div>
+          <h1>A Step</h1>
+          <h3>{JSON.stringify(context)}</h3>
+          <button onClick={() => history.push('B', { a: 'a' })}>Next</button>
+        </div>
+      )}
+      B={({ context, history }) => (
+        <div>
+          <h1>B Step</h1>
+          <p>a: {context.a}</p>
+          <BFunnel a={context.a} onNext={b => history.push('C', { b })} />
+        </div>
+      )}
+      C={({ context }) => (
+        <div>
+          <h1>C Step</h1>
+          <p>a: {context.a}</p>
+          <p>b: {context.b}</p>
+        </div>
+      )}
+    />
   );
 };
 
