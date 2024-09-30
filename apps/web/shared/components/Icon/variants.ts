5a0fe3cb4fcd3789afada0c8b32573e36a2af7d1
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-10-01 00:32:20 +0900

    feat: add icon motion variant

--- apps/web/shared/components/Icon/variants.ts
+++ apps/web/shared/components/Icon/variants.ts
@@ -18,4 +18,38 @@ export const ICON_VARIANTS = {
       },
     }),
   } as Variants,
+  NAVIGATE: (direction: 'back' | 'front') =>
+    (direction === 'back'
+      ? {
+          closed: () => ({
+            rotate: '-135deg',
+            transition: {
+              duration: 0.1,
+              bounce: 1,
+            },
+          }),
+          hover: () => ({
+            rotate: '-90deg',
+            transition: {
+              duration: 0.1,
+              bounce: 1,
+            },
+          }),
+        }
+      : {
+          closed: () => ({
+            rotate: '45deg',
+            transition: {
+              duration: 0.1,
+              bounce: 1,
+            },
+          }),
+          hover: () => ({
+            rotate: '0deg',
+            transition: {
+              duration: 0.1,
+              bounce: 1,
+            },
+          }),
+        }) as Variants,
 };
