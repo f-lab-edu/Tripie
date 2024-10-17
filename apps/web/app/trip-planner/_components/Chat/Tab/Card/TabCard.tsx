59f205c4dcf9e7d709e6a9b7d165b82f54d239d5
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-10-18 00:54:46 +0900

    feat: add Chat Tab Card component

---
+++ apps/web/app/trip-planner/_components/Chat/Tab/Card/TabCard.tsx
@@ -0,0 +1,65 @@
+'use client';
+import classNames from 'classnames/bind';
+
+import { Container, Headings } from '@tripie-pyotato/design-system';
+
+import Style from './tab-card.module.scss';
+
+import { Activity, TripContent } from 'models/Aws';
+import { useContext, useEffect, useRef } from 'react';
+import Card from 'shared/components/Card/Card';
+import Chip from 'shared/components/Chip/Chip';
+import Divider from 'shared/components/Divider/Divider';
+import { TabContext } from '../..';
+
+const cx = classNames.bind(Style);
+
+const TabCard = ({
+  label,
+  trip,
+  index,
+  activity,
+  comments,
+}: {
+  label: Activity['label'];
+  trip: TripContent;
+  index: number;
+  activity: string;
+  comments: string;
+}) => {
+  const ref = useRef<HTMLDivElement | null>(null);
+
+  // 날짜별 일자 중의 선택한 일정
+  const { current, cycle } = useContext(TabContext);
+
+  // 선택한 일정이 변경되면 리스트 중 해당 카드로 스크롤하여 보여줍니다.
+  useEffect(() => {
+    if (ref.current) {
+      ref.current?.scrollIntoView({
+        behavior: 'smooth',
+        block: 'center',
+      });
+    }
+  }, [current]);
+
+  return (
+    <Card.ClickableContent
+      key={activity}
+      className={cx('trip-activity', 'card-item', current === `${trip.day}-${index}` ? 'selected' : null)}
+      selected={current === `${trip.day}-${index}`}
+      onClick={() => {
+        cycle(`${trip.day}-${index}`);
+      }}
+    >
+      <Container margin="none" className={cx('title')}>
+        <div ref={current === `${trip.day}-${index}` ? ref : null} />
+        <Chip className={cx(label, 'label')}>{index + 1}</Chip>
+        <Headings.H4 className={cx('accented')}>{activity}</Headings.H4>
+      </Container>
+      <Divider />
+      {comments}
+    </Card.ClickableContent>
+  );
+};
+
+export default TabCard;
