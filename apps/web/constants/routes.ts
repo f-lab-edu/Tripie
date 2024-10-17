68fc127eec1f5a3ece6fabf1898dd9588a3a8d4b
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-10-17 18:12:59 +0900

    feat: add constants

--- apps/web/constants/routes.ts
+++ apps/web/constants/routes.ts
@@ -4,6 +4,15 @@ const ROUTES = {
     CHATGPT: 'chat/completions',
     SEARCH_PLACE: 'place/textsearch/json',
     CONTINENTL: 'continentl/country-list',
+    SERVER_TIME: 'server-time',
+    PLACE_SEARCH: 'v1/places:searchText',
+    PLACES: 'places',
+    PLACE_PHOTOS: 'place-photos',
+    OPEN_STREET_MAP: 'open-street-map',
+    LOCATION: 'location',
+    AWS_LOCATION_TEXT: 'aws/text',
+    AWS_LOCATION_SUGGESTED_PLACES: 'aws/suggested-places',
+    AWS_LOCATION_BY_PLACE_ID: 'aws/place-by-id-places',
   },
   PAGE: {
     LANDING: [
@@ -17,11 +26,16 @@ const ROUTES = {
     HOME: { label: 'Home', href: '/' },
     SERVICES: { label: 'Services', href: '/#Services' },
     CONTACT: { label: 'Contact', href: '/#Contact' },
+    TRIP_PLANNER: { label: 'Trip Planner', href: '/trip-planner' },
   },
   RESOURCE: {
     ARROW: { label: 'icon', src: '/arrow.png' },
     CHECK: { label: 'icon', src: '/check.png' },
     X: { label: 'icon', src: '/x.png' },
+    REFRESH: { label: 'icon', src: '/refresh.png' },
+    PLANE: { label: 'icon', src: '/plane.png' },
+    CLOUD: { label: 'icon', src: '/cloud.png' },
+    LOADING: { label: 'icon', src: '/loading.png' },
   },
 };
 
