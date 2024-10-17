b2230eed879bfb92476d3dd38bd8b56502b01b42
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-10-18 00:41:12 +0900

    feat: add DurationFunnel component

--- apps/web/app/trip-planner/_components/Duration/index.tsx
+++ apps/web/app/trip-planner/_components/Duration/index.tsx
@@ -1,16 +1,24 @@
+'use client';
 import { Container } from '@tripie-pyotato/design-system';
 import classNames from 'classnames/bind';
 import ROUTES from 'constants/routes';
-import { ContinentKeys } from 'hooks/useCountries';
 import Icon from 'shared/components/Icon/Icon';
 import Calendar from './Calendar';
 
+import { ContinentKeys } from 'models/Continent';
 import Style from './duration.module.scss';
 
 const cx = classNames.bind(Style);
 
 interface Props {
-  context?: { continent: ContinentKeys; country: string; city: string; duration: string };
+  context: {
+    continent: ContinentKeys;
+    country: string;
+    city: { all: string[]; selected: string[] };
+    duration?: string;
+    companion?: string;
+    preference?: string;
+  };
   onNext: (b: string) => void;
 }
 
@@ -26,7 +34,7 @@ const DurationFunnel = ({ context, onNext }: Props) => {
         </h2>
       </Container>
 
-      <Calendar onNext={onNext} />
+      <Calendar onNext={onNext} context={context} />
     </>
   );
 };
