d368ccabcb2f2aee9e67341a4cb396a157d3bb1b
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-10-18 00:35:44 +0900

    feat: add Country step components

--- apps/web/app/trip-planner/_components/Location/Countries/CountryDetail.tsx
+++ apps/web/app/trip-planner/_components/Location/Countries/CountryDetail.tsx
@@ -1,39 +1,62 @@
+import { Headings } from '@tripie-pyotato/design-system';
 import { Continentl } from 'app/api/firebase/getContinentl';
+import classNames from 'classnames/bind';
+import useContinentl from 'hooks/query/useContinentl';
+import Card from 'shared/components/Card/Card';
+import Chip from 'shared/components/Chip/Chip';
+import List from 'shared/components/List/List';
+import Style from './countries.module.scss';
+
+const cx = classNames.bind(Style);
 
 const Detail = ({ data }: { data: Continentl }) => {
   return (
-    <>
-      수도: {data.capital}
-      지역:
-      {data.states != null ? (
-        <ul>
-          {data.states.map((state: string) => (
-            <li key={state}>{state}</li>
-          ))}
+    <Card>
+      <List className={cx('list-wrap')}>
+        <li className={cx('list-item')}>
+          <Headings.H2>{data.name}</Headings.H2> <Headings.H4 className={cx('accented')}>({data.code})</Headings.H4>
+        </li>
+        <ul className={cx('info-wrap')}>
+          <li className={cx('country-flag')}>
+            {data['flag-image']?.[0] != null ? (
+              <img className={cx('flex-item', 'flag-image')} src={data['flag-image'][0]} alt={data['flag-image'][0]} />
+            ) : (
+              'loading..'
+            )}
+          </li>
+          <li className={cx('info-text')}>
+            <ul>
+              <li className={cx('list-item')}>
+                <Headings.H3 className={cx('accented', 'capital')}>수도</Headings.H3>
+                <div className={cx('capital-name')}>{data.capital}</div>
+              </li>
+              <li className={cx('list-item')}>
+                <Headings.H3 className={cx('accented', 'language')}>공식 언어</Headings.H3>
+                {data.official_language != null ? (
+                  <List className={cx('flex')}>
+                    {data?.official_language.map((language: string) => (
+                      <li key={language}>
+                        <Chip>{language}</Chip>
+                      </li>
+                    ))}
+                  </List>
+                ) : null}
+              </li>
+            </ul>
+          </li>
         </ul>
-      ) : null}
-      나라 코드 : {data.code}
-      지역 : {data.region}
-      공식 언어:
-      {data.official_language != null ? (
-        <ul>{data?.official_language.map((language: string) => <li key={language}>{language}</li>)}</ul>
-      ) : null}
-      국기:
-      {data['flag-image']?.[0] != null ? <img src={data['flag-image'][0]} alt={data['flag-image'][0]} /> : null}
-    </>
+      </List>
+    </Card>
   );
 };
 
-const CountryDetail = ({ selectedCountry, data }: { selectedCountry: string; data: Continentl | Continentl[] }) => {
+const CountryDetail = ({ selectedCountry }: { selectedCountry: string }) => {
+  const { data } = useContinentl(selectedCountry);
+
   if (data == null) {
-    return <>data is null</>;
+    return <div>{selectedCountry}</div>;
   }
 
-  return (
-    <>
-      <div>{selectedCountry}</div>
-      <Detail data={Array.isArray(data) ? data[0] : data} />
-    </>
-  );
+  return <Detail data={data[0]} />;
 };
 export default CountryDetail;
---
+++ apps/web/app/trip-planner/_components/Location/Countries/index.tsx
@@ -0,0 +1,99 @@
+import { useQueryClient } from '@tanstack/react-query';
+import { Container } from '@tripie-pyotato/design-system';
+import getContinentlList from 'app/api/firebase/getContinentl';
+import Loading from 'app/home/_components/loading/loading';
+import classNames from 'classnames/bind';
+import ROUTES from 'constants/routes';
+import useContinentl from 'hooks/query/useContinentl';
+import useCountries from 'hooks/query/useCountries';
+import { ContinentKeys } from 'models/Continent';
+import { Country } from 'models/Country';
+import { useRef, useState } from 'react';
+import AnimatedButton from 'shared/components/Button/AnimatedButton';
+import Icon from 'shared/components/Icon/Icon';
+import CountryDetail from './CountryDetail';
+import { CountryList } from './List/CountryList';
+import Style from './countries.module.scss';
+
+interface Props {
+  context: { continent: ContinentKeys; country?: string; city?: { all: string[]; selected: string[] } };
+  onNext: (country: {
+    continent?: ContinentKeys;
+    country?: string;
+    city?: { all: string[]; selected: string[] };
+    duration?: string;
+  }) => void;
+}
+
+const cx = classNames.bind(Style);
+
+export function CountryStep({ context, onNext }: Props) {
+  const { data, isLoading } = useCountries(context.continent == null ? 'all' : context.continent);
+  const [selectedCountry, setSelectedCountry] = useState(context?.country == null ? '' : context.country);
+  /** 국가 선택 시 다음으로 스크롤 */
+  const bottomRef = useRef<HTMLDivElement | null>(null);
+  const queryClient = useQueryClient();
+
+  // 국가 내의 도시/시 정보 가져오기
+  const getCitys = (country: string) => {
+    return queryClient.ensureQueryData({
+      queryKey: useContinentl.queryKey(country),
+      queryFn: () =>
+        getContinentlList().then(countries => {
+          if (country === 'all') {
+            return countries;
+          } else {
+            return countries?.filter(place => place.id === country);
+          }
+        }),
+    });
+  };
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  return (
+    <>
+      <Container margin="none">
+        <Container margin="none">
+          <Icon.Navigate src={ROUTES.RESOURCE.ARROW['src']} />
+        </Container>
+        <h2>
+          떠나고 싶은 <span className={cx('accented')}>나라</span>는?
+        </h2>
+      </Container>
+
+      <CountryList
+        countries={data}
+        bottomRef={bottomRef}
+        setSelectedCountry={setSelectedCountry}
+        selectedCountry={selectedCountry}
+      />
+      <CountryDetail
+        selectedCountry={
+          // 선택한 나라가 없을 경우
+          selectedCountry === '' || !data.some((country: Country) => country.name === selectedCountry)
+            ? data[0].name
+            : selectedCountry
+        }
+      />
+      <Container margin="none">
+        <AnimatedButton
+          withBorder={true}
+          onClick={async () => {
+            const states = await getCitys(selectedCountry);
+            onNext({ country: selectedCountry, city: { all: states?.[0]?.states, selected: [] } });
+          }}
+          className={cx('submit-button')}
+        >
+          <Container margin="none" className={cx('flex')}>
+            "{selectedCountry}"로 보기 <Icon src={ROUTES.RESOURCE.ARROW['src']} />
+          </Container>
+        </AnimatedButton>
+      </Container>
+
+      <section ref={bottomRef} />
+    </>
+  );
+}
