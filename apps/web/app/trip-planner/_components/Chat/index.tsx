1c1627f5fd0747ee50a07f64ea3c85e50e56ddd3
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-10-18 01:02:19 +0900

    feat: add chat funnel step component

---
+++ apps/web/app/trip-planner/_components/Chat/index.tsx
@@ -0,0 +1,124 @@
+'use client';
+import classNames from 'classnames/bind';
+
+import { Container } from '@tripie-pyotato/design-system';
+
+import { MAX_TOKEN } from 'constants/chat';
+import ROUTES from 'constants/routes';
+import useChat from 'hooks/query/useChat';
+import { Activity, AwsPlace, AwsPlaceResult } from 'models/Aws';
+import { ContinentKeys } from 'models/Continent';
+import { TripieUser } from 'models/User';
+import { signIn, useSession } from 'next-auth/react';
+import { Dispatch, SetStateAction, createContext, useMemo, useState } from 'react';
+import Icon from 'shared/components/Icon/Icon';
+import AwsMap from './AwsMap';
+import ChatTab from './Tab';
+import Style from './chat.module.scss';
+
+const cx = classNames.bind(Style);
+
+export interface ChatFunnelProps {
+  context: {
+    continent: ContinentKeys;
+    country: string;
+    city: { all: string[]; selected: string[] };
+    duration: string;
+    companion: string;
+    preference: string;
+  };
+}
+
+export type LocationMarker = {
+  lat: number;
+  lng: number;
+  label: Activity['label'];
+  info: string;
+  index: string;
+  parent: string;
+};
+export type AwsLocationWithLabel = {
+  label: AwsPlace['Label'];
+} & AwsPlaceResult;
+
+/** props drilling 완화를 위해 컨텍스트로 state 관리. 전역으로 사용될 state는 아니기 때문에 간단히 내장 useContext 사용
+ * 선택한 tab의 일정, `{일정 날짜}-{선택 일정 인덱스}`
+ */
+export const TabContext = createContext<{ current: string; cycle: Dispatch<SetStateAction<string>> }>({
+  current: '1-0',
+  cycle: () => null,
+});
+
+/** props drilling 완화를 위해 컨텍스트로 state 관리. 전역으로 사용될 state는 아니기 때문에 간단히 내장 useContext 사용
+ * 선택한 여행 일정 일짜
+ */
+export const SelectedDateContext = createContext<{ currentDate: number; dateCycle: Dispatch<SetStateAction<number>> }>({
+  currentDate: 0,
+  dateCycle: () => null,
+});
+
+const ChatFunnel = ({ context }: ChatFunnelProps) => {
+  const { data, isLoading } = useChat(context);
+  const { status, data: userData } = useSession();
+
+  // 여행 일정 중 선택한 날짜
+  const [selectedDate, setSelectedDate] = useState<number>(0);
+  // 여행 일정 중 선택한 일정
+  const [selectedActivity, setSelectedActivity] = useState(`${selectedDate}-0`);
+
+  // selectedActivity만 바뀔때마다 리렌더되도록
+  const selectedActivityValues = useMemo(() => {
+    return { current: selectedActivity, cycle: setSelectedActivity };
+  }, [selectedActivity]);
+
+  // selectedDate만 바뀔때마다 리렌더되도록
+  const selectedDateValues = useMemo(() => {
+    return { currentDate: selectedDate, dateCycle: setSelectedDate };
+  }, [selectedDate]);
+
+  // console.log(userData);
+
+  if (status === 'unauthenticated') {
+    signIn();
+  }
+
+  return (
+    <TabContext.Provider value={selectedActivityValues}>
+      <SelectedDateContext.Provider value={selectedDateValues}>
+        <Container margin="none">
+          <Container margin="none">
+            {isLoading ? (
+              <Icon.Loading />
+            ) : (userData?.user as TripieUser)?.isAdmin ? (
+              <>
+                <Icon.Navigate src={ROUTES.RESOURCE.ARROW['src']} />
+              </>
+            ) : (
+              <>
+                <Icon.Navigate src={ROUTES.RESOURCE.ARROW['src']} />{' '}
+                {MAX_TOKEN > (userData?.user as TripieUser)?.usedGptToken
+                  ? `${MAX_TOKEN - (userData?.user as TripieUser)?.usedGptToken}토큰이 남았습니다.`
+                  : `토큰을 모두 사용하셨습니다.`}
+              </>
+            )}
+          </Container>
+          <h2>
+            <span className={cx('accented')}>Chat</span>
+          </h2>
+        </Container>
+        <Container margin="none" className={cx('trip-content-wrap')}>
+          {isLoading ? (
+            <></>
+          ) : (
+            <>
+              <ChatTab data={data.plans} />
+              <AwsMap plans={data.plans} locations={data.locations} />
+            </>
+          )}
+        </Container>
+      </SelectedDateContext.Provider>
+    </TabContext.Provider>
+  );
+};
+
+export default ChatFunnel;
