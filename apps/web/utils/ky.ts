385bc2458caef4f37f287f848d66ccdb6df02f7b
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-10-17 17:53:49 +0900

    feat: ky setup

---
+++ apps/web/utils/ky.ts
@@ -0,0 +1,38 @@
+import ROUTES from 'constants/routes';
+import ky from 'ky';
+
+const api = ky.extend({
+  prefixUrl: ROUTES.API.BASE,
+  hooks: {
+    afterResponse: [
+      (_, __, res) => {
+        console.log(res?.status);
+        if (res?.status === 401) {
+          console.log('invalid token');
+        } else if (res?.status === 429) {
+          console.log('too many requests..');
+        }
+        console.log(res);
+      },
+    ],
+  },
+});
+
+export const awsApi = ky.extend({
+  prefixUrl: 'http://localhost:3000' + ROUTES.API.BASE,
+  hooks: {
+    afterResponse: [
+      (_, __, res) => {
+        console.log(res?.status);
+        if (res?.status === 401) {
+          console.log('invalid token');
+        } else if (res?.status === 429) {
+          console.log('too many requests..');
+        }
+        console.log(res);
+      },
+    ],
+  },
+});
+
+export default api;
