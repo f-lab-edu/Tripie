9ab1a21461d8d63c5542ca6758c0d648cf11af56
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-10-17 18:00:39 +0900

    chore: move replaced google api map provider to playground

--- apps/web/provider/MapProvider.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import { APIProvider } from '@vis.gl/react-google-maps';
-import { API_KEY } from 'constants/maps';
-import { ReactNode } from 'react';
-
-export function MapProvider({ children }: { children: ReactNode }) {
-  return <APIProvider apiKey={API_KEY}>{children}</APIProvider>;
-}
