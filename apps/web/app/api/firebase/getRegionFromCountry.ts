5697cf31b5820c93c4c598b6073a8af5a536d6b1
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-10-18 12:10:19 +0900

    chore: rename argument name

--- apps/web/app/api/firebase/getRegionFromCountry.ts
+++ apps/web/app/api/firebase/getRegionFromCountry.ts
@@ -3,10 +3,10 @@ import { query, where } from 'firebase/firestore';
 import { collection } from 'firebase/firestore';
 import db from 'firebase/store';
 
-const getRegionFromCountry = async (preference: string[]) => {
+const getRegionFromCountry = async (regions: string[]) => {
   const citiesRef = collection(db, 'cities');
   try {
-    const q = query(citiesRef, where('regions', 'array-contains-any', preference));
+    const q = query(citiesRef, where('regions', 'array-contains-any', regions));
     return q;
   } catch (e) {
     console.error('Error listing document: ', e);
