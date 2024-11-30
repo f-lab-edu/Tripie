c52815857862749c052e1769c9d1dbf523ff81ef
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-11-30 14:52:45 +0900

    feat: add constants

---
+++ apps/web/constants/faq.ts
@@ -0,0 +1,32 @@
+const FAQS = [
+  {
+    tag: "What's unique about Tripie?",
+    header: "What's unique about Tripie?",
+    details:
+      "Our extremely flexible development subscription is what set's us apart in the AI-development space. We fully understand that your business needs differ every month in this fast paced-environment. That's why we created our innovative subscription model.",
+  },
+  {
+    tag: 'How many builds can I request each month?',
+    header: 'How many builds can I request each month?',
+    details:
+      'You can request unlimited builds and unlimited revisions, your developers are ready to transform your ideas into reality!',
+  },
+  {
+    tag: 'Can I cancel my subscription at any time?',
+    header: 'Can I cancel my subscription at any time?',
+    details: 'Yes, you can pause or cancel your subscription whenever you want!',
+  },
+  {
+    tag: 'How secure are your solutions?',
+    header: 'How secure are your solutions?',
+    details:
+      'Security is paramount. We adhere to best practices and industry standards to protect your data and privacy.',
+  },
+  {
+    tag: 'Can your solutions scale with my business growth?',
+    header: 'Can your solutions scale with my business growth?',
+    details: 'Definitely. Both our solutions and our subscriptions are designed to handle your business growth.',
+  },
+];
+
+export default FAQS;
---
+++ apps/web/constants/resources.ts
@@ -0,0 +1,19 @@
+// 이미지 리소스 경로
+const RESOURCE = {
+  ARROW: '/arrow.png',
+  CHECK: '/check.png',
+  X: '/x.png',
+  REFRESH: '/refresh.png',
+  PLANE: '/refresh.png',
+  CLOUD: '/cloud.png',
+  LOADING: '/loading.png',
+  TRAM: '/tram.png',
+  TRAIN: '/train.png',
+  SHIP: '/ship.png',
+  BUS: '/bus.png',
+  CAR: '/car.png',
+  FLAG: '/flag.png',
+  PLACEHOLDER: 'https://placehold.co/600x600?text=NO IMAGE',
+};
+
+export default RESOURCE;
