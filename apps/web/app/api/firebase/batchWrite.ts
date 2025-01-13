de02823530d0340efef9db9bf00c6cac0f06b461
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2025-01-13 18:12:21 +0900

    chore: remove unused type

--- apps/web/app/api/firebase/batchWrite.ts
+++ apps/web/app/api/firebase/batchWrite.ts
@@ -2,8 +2,6 @@ import { doc, writeBatch } from 'firebase/firestore';
 // import db from 'firebase/store';
 import db from '../../../firebase/store';
 
-type Content = { day: number; order: number; contentId: string; text: string };
-
 const batchWrite = async ({ collection, document, items }: { collection: string; document: string; items: Object }) => {
   const batch = writeBatch(db);
   //   const docRef = doc(db, collection, document);
