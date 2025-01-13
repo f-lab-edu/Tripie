bbc7f287f16f0c1441f5309ea5369128d5f754c7
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2025-01-13 17:50:18 +0900

    feat: fix directory path

--- apps/web/app/api/firebase/getContinentl.ts
+++ apps/web/app/api/firebase/getContinentl.ts
@@ -1,5 +1,6 @@
 import { collection, getDocs } from 'firebase/firestore';
-import db from 'firebase/store';
+// import db from 'firebase/store';
+import db from '../../../firebase/store';
 
 export type Continentl = {
   code: string;
