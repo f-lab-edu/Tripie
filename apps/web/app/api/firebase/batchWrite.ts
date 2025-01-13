bb2ce8d242ce211799b79f1c904fb284b1bcba38
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2025-01-13 18:15:25 +0900

    chore: comment out code

--- apps/web/app/api/firebase/batchWrite.ts
+++ apps/web/app/api/firebase/batchWrite.ts
@@ -1,14 +1,14 @@
-import { doc, writeBatch } from 'firebase/firestore';
-// import db from 'firebase/store';
-import db from '../../../firebase/store';
+// import { doc, writeBatch } from 'firebase/firestore';
+// // import db from 'firebase/store';
+// import db from '../../../firebase/store';
 
-const batchWrite = async ({ collection, document, items }: { collection: string; document: string; items: Object }) => {
-  const batch = writeBatch(db);
-  //   const docRef = doc(db, collection, document);
-  const docRef = doc(db, collection);
+// const batchWrite = async ({ collection, document, items }: { collection: string; document: string; items: Object }) => {
+//   const batch = writeBatch(db);
+//   //   const docRef = doc(db, collection, document);
+//   const docRef = doc(db, collection);
 
-  batch.set(docRef, items);
-  await batch.commit();
-};
+//   batch.set(docRef, items);
+//   await batch.commit();
+// };
 
-export default batchWrite;
+// export default batchWrite;
