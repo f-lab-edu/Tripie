4ed982f16cd5d1b838cf1a6b3c600d325f35fcea
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2025-01-13 17:54:27 +0900

    chore: remove playground contents

--- apps/web/app/playground/regions/[regionId]/_shared/_sections/AttractionTitle.tsx
+++
@@ -1,7 +0,0 @@
-'use server';
-import { AttractionArticle } from 'models/Attraction';
-
-const AttractionTitle = ({ primary }: { primary: AttractionArticle['source']['names']['primary'] }) => {
-  return <h2>{primary}</h2>;
-};
-export default AttractionTitle;
