10f3db90373d6563fd98463b2e11a86b2dfdfbec
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2025-01-19 22:42:13 +0900

    fix: add CustomUser type

--- apps/web/hooks/useChatToken.ts
+++ apps/web/hooks/useChatToken.ts
@@ -2,11 +2,14 @@
 
 import incrementUsedGptToken from 'app/api/gpt/incrementUsedGptToken';
 import { ChatFunnelProps } from 'app/trip-planner/_components/Chat';
+import { Session } from 'next-auth';
 import { useSession } from 'next-auth/react';
 import { useCallback } from 'react';
 
+type CustomUser = { data: { user: Session['user'] & { usedGptToken: number } } };
+
 const useChatToken = ({ context }: ChatFunnelProps) => {
-  const { data } = useSession();
+  const { data } = useSession() as unknown as CustomUser;
 
   useCallback(() => {
     if (data?.user?.email != null) {
@@ -18,6 +21,6 @@ const useChatToken = ({ context }: ChatFunnelProps) => {
     }
   }, [data?.user?.email, context]);
 
-  return { tokenData: data?.user?.usedGptToken };
+  return { tokenData: data?.user.usedGptToken };
 };
 export default useChatToken;
