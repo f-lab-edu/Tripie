d23a365edfd94e2052141d1f84079c3cbefbbb7e
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2025-01-20 15:18:47 +0900

    feat: update turbo

--- apps/web/app/api/auth/prisma.ts
+++
@@ -1,8 +0,0 @@
-// @ts-ignore
-import { PrismaClient } from '@tripie-pyotato/db';
-
-const globalForPrisma = globalThis as unknown as { prisma: typeof PrismaClient };
-
-export const prisma = globalForPrisma.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
