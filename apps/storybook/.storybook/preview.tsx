2abe97938aa4eab576d14cee40057be1c3f37eee
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2025-02-05 18:35:42 +0900

    chore: add dark settings

--- apps/storybook/.storybook/preview.tsx
+++ apps/storybook/.storybook/preview.tsx
@@ -1,7 +1,6 @@
 import type { Preview } from '@storybook/react';
 
 import '@tripie-pyotato/design-system/global';
-import React from 'react';
 
 const preview: Preview = {
   parameters: {
@@ -11,38 +10,32 @@ const preview: Preview = {
         date: /Date$/i,
       },
     },
-  },
-  decorators: [
-    (Story, context) => {
-      return (
-        <>
-          <div className="background-container">
-            <div className="stars"></div>
-            <div className="twinkling"></div>
-          </div>
-          <div className="layout-wrap">
-            <Story />
-          </div>
-        </>
-      );
+    backgrounds: {
+      values: [
+        // 👇 Default values
+        { name: 'Dark', value: '#080807' },
+        { name: 'Light', value: '#F7F9F2' },
+      ],
+      // 👇 Specify which background is shown by default
+      default: 'Dark',
     },
-  ],
+  },
 
-  globalTypes: {
-    theme: {
-      description: 'Global theme for components',
-      defaultValue: 'light',
-      toolbar: {
-        // The label to show for this toolbar item
-        title: 'Theme',
-        icon: 'circlehollow',
-        // Array of plain string values or MenuItem shape (see below)
-        items: ['light', 'dark'],
-        // Change title based on selected value
-        dynamicTitle: true,
-      },
-    },
-  },
+  // globalTypes: {
+  //   theme: {
+  //     description: 'Global theme for components',
+  //     defaultValue: 'light',
+  //     toolbar: {
+  //       // The label to show for this toolbar item
+  //       title: 'Theme',
+  //       icon: 'circlehollow',
+  //       // Array of plain string values or MenuItem shape (see below)
+  //       items: ['light', 'dark'],
+  //       // Change title based on selected value
+  //       dynamicTitle: true,
+  //     },
+  //   },
+  // },
 };
 
 export default preview;
