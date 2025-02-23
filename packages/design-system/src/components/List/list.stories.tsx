1c1573510f9c8d6d6bfe12147bba9e70b6c75e57
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2025-02-23 13:00:47 +0900

    docs: withBorder false for list stories

--- packages/design-system/src/components/List/list.stories.tsx
+++ packages/design-system/src/components/List/list.stories.tsx
@@ -35,6 +35,7 @@ const Children = () =>
         'https://media.triple.guide/triple-cms/c_limit,f_auto,h_2048,w_2048/54e3594d-1087-43ff-b760-7231d4103edf.jpeg의 이미지일 수도 있음'
       }
       summary={'대자연부터 감성 카페까지, 인생샷을 남길 수 있는 곳'}
+      withBorder={false}
       title={'여름 제주 인생샷 명소 BEST 7'}
       key={index}
     />
