5c8d12ed2e5f5806935f77bf6444f7d2d83cef9e
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2025-02-25 14:34:22 +0900

    feat: add meta tag for attraction detail page

--- apps/web/app/regions/[regionId]/attractions/[articleId]/page.tsx
+++ apps/web/app/regions/[regionId]/attractions/[articleId]/page.tsx
@@ -4,12 +4,42 @@ import { Card, Container, TripieImage } from '@tripie-pyotato/design-system';
 import getArticleDetail from 'app/api/articles/detail';
 import AttractionTitle from 'app/regions/_components/shared/_sections/AttractionTitle';
 import classNames from 'classnames/bind';
+import API from 'constants/api-routes';
+import ROUTE from 'constants/routes';
+import { Metadata, ResolvingMetadata } from 'next';
 import RegionBody from '../../../_components/RegionBody';
 import Style from './attractions.module.scss';
 
 const cx = classNames.bind(Style);
 
-const Attractions = async ({ params }: { params: Promise<{ regionId: string; articleId: string }> }) => {
+type Props = {
+  params: Promise<{ regionId: string; articleId: string }>;
+};
+
+export async function generateMetadata({ params }: Props, parent: ResolvingMetadata): Promise<Metadata> {
+  const regionId = (await params).regionId;
+  const articleId = (await params).articleId;
+
+  const { data } = await getArticleDetail('attraction', regionId, articleId);
+
+  const previousImages = (await parent).openGraph?.images || [];
+
+  const title =
+    data?.source.names.primary ?? data?.source.names.ko ?? data?.source.names.en ?? data?.source.names.local ?? '';
+  return {
+    title: `✈️Tripie | ${title}`,
+    openGraph: {
+      images: [data?.source.image.sizes.full.url ?? '', ...previousImages],
+      type: 'website',
+      url: `${API.BASE_URL}${ROUTE.REGIONS.href}/${regionId}/attractions/${articleId}`,
+      title: `${title}`,
+      description: `${data?.source.comment}`,
+      siteName: 'Tripie',
+    },
+  };
+}
+
+const Attractions = async ({ params }: Props) => {
   const { blurredThumbnail, data } = await getArticleDetail(
     'attraction',
     (await params).regionId,
