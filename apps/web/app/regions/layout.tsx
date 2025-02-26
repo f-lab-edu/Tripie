91389af757f2d7f3835a8b19a4c034d62f4e2679
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2025-02-26 13:59:15 +0900

    chore: remove default layout metadata

--- apps/web/app/regions/layout.tsx
+++ apps/web/app/regions/layout.tsx
@@ -1,20 +1,12 @@
 import '@tripie-pyotato/design-system/global';
-import type { Metadata } from 'next';
 
 import { Container } from '@tripie-pyotato/design-system';
 import Nav from 'app/home/_components/nav/Nav';
 import classNames from 'classnames/bind';
 import { ReactNode } from 'react';
 import Style from './layout.module.scss';
-import { sharedMetaData } from './shared-metadata';
 
 const cx = classNames.bind(Style);
-
-export const metadata: Metadata = {
-  title: sharedMetaData?.title,
-  description: sharedMetaData?.description,
-  openGraph: sharedMetaData,
-};
 
 export default function Layout({
   children,
