aef64a976bff095be99cbf2cd5954fc5bfa6ac81
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2025-03-12 11:56:20 +0900

    feat: move redirect path rules from next.config to middleware

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -5,9 +5,52 @@ export function middleware(request: NextRequest) {
   const requestHeaders = new Headers(request.headers);
   requestHeaders.set('x-pathname', request.nextUrl.pathname);
 
+  const { pathname } = request.nextUrl;
+
+  // dev 일 경우에만 playground 경로 접근
+  if (process.env.NODE_ENV === 'production' && pathname.startsWith('/playground')) {
+    return NextResponse.redirect('/home');
+  }
+
+  // '/' 경로는 /home으로 redirect
+  if (pathname === '/') {
+    return NextResponse.redirect('/home');
+  }
+
+  // Redirect specific API routes dynamically
+  if (pathname.startsWith('/api/continentl/country-list')) {
+    return NextResponse.redirect('https://continentl.com/api/country-list');
+  }
+
+  // aws 위치 검색 : 검색어로 찾기
+  if (pathname.startsWith('/api/aws/text')) {
+    return NextResponse.redirect(
+      'https://places.geo.ap-northeast-1.amazonaws.com/places/v0/indexes/explore.place.Esri/search/text'
+    );
+  }
+
+  // 유사 검색어로 찾은 placeId로 위치 검색
+  // https://docs.aws.amazon.com/location/latest/APIReference/API_GetPlace.html
+  // https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/client/location/command/GetPlaceCommand/
+  if (pathname.startsWith('/api/place')) {
+    return NextResponse.redirect('https://tripie-server.vercel.app/api/trip-advisor');
+  }
+
+  // 검색어 매칭 없을 경우: 유사 검색어로 찾기
+  if (pathname.startsWith('/api/aws/suggested-places')) {
+    return NextResponse.redirect(
+      'https://places.geo.ap-northeast-1.amazonaws.com/places/v0/indexes/explore.place.Esri/search/suggestions'
+    );
+  }
+
   return NextResponse.next({
     request: {
       headers: requestHeaders,
     },
   });
 }
+
+// Apply middleware only to API routes
+export const config = {
+  matcher: ['/api/:path*'], // Runs middleware for all `/api/*` requests
+};
