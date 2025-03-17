9ae0bfe167f926894c61f3f946f33790fba13984
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2025-03-17 12:40:34 +0900

    feat: hide ai/auth button on signin page

--- apps/web/app/home/_components/nav/Nav.tsx
+++ apps/web/app/home/_components/nav/Nav.tsx
@@ -4,7 +4,7 @@ import Style from './nav.module.scss';
 
 import { Icon, MenuToggle, NoStyleButton, Text } from '@tripie-pyotato/design-system';
 import ROUTE, { LANDING_SECTION } from 'constants/routes';
-import { useRouter } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 
 import Link from 'next/link';
 
@@ -15,6 +15,7 @@ const cx = classNames.bind(Style);
 
 const Nav = () => {
   const navigate = useRouter();
+  const pathName = usePathname();
 
   return (
     <MenuToggle>
@@ -34,8 +35,15 @@ const Nav = () => {
           <Link href={ROUTE.REGIONS.href}>{ROUTE.REGIONS.label}</Link>
           <Icon />
         </MenuToggle.Item>
-        <AiTripButton />
-        <AuthButton />
+
+        {/** 로그인 페이지에 있을 경우 버튼 표시 X */}
+        {pathName === ROUTE.SIGN_IN.href ? null : (
+          <>
+            <AiTripButton />
+            <AuthButton />
+          </>
+        )}
+
         {process.env.NODE_ENV === 'development' ? (
           <MenuToggle.Item key={'dev'}>
             <Link href={ROUTE.PLAYGROUND.href}>{ROUTE.PLAYGROUND.label}</Link>
