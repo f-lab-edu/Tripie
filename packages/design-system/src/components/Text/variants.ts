7054619274a8b0a95a402b00d3a0114d1dc1552a
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2025-03-18 12:11:22 +0900

    Error: CSS: duration: Property duration doesn't exist.

--- packages/design-system/src/components/Text/variants.ts
+++ packages/design-system/src/components/Text/variants.ts
@@ -2,16 +2,18 @@ import { MotionProps } from '../../shared/wrappers/motion-wrapper';
 
 export const VARIANTS = {
   FLICK_TEXT: {
-    rest: { opacity: 1, y: '0', duration: 0.5 },
+    rest: { opacity: 1, y: '0', 
+    // duration: 0.5 
+  },
     hover: {
       opacity: 1,
       y: '-3rem',
       transition: {
-        duration: 0.5,
+        bounce:0
+        // duration: 0.5,
       },
     },
-  } as MotionProps['variants'],
-
+  },
   TEXT: {
     rest: { opacity: 1, y: '2rem', duration: 0.5 },
     hover: {
@@ -21,19 +23,18 @@ export const VARIANTS = {
         duration: 0.5,
       },
     },
-  } as MotionProps['variants'],
+  },
 
-  TITLE: (restY: number, hoverY: number) =>
-    ({
-      rest: { opacity: 1, y: restY, duration: 0.5 },
-      hover: {
-        opacity: 1,
-        y: hoverY,
-        transition: {
-          duration: 0.5,
-        },
+  TITLE: (restY: number, hoverY: number) => ({
+    rest: { opacity: 1, y: restY, duration: 0.5 },
+    hover: {
+      opacity: 1,
+      y: hoverY,
+      transition: {
+        duration: 0.5,
       },
-    }) as MotionProps['variants'],
+    },
+  }),
   SLIDE: (duration: number) =>
     ({
       rest: () => ({}),
