bcd076c8c78e9c4dfa8fab4f78f26c8ad4ec211f
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2025-03-18 14:24:22 +0900

    Warning: Section lacks heading. Consider using h2-h6 elements to add identifying headings to all sections, or else use a div element instead for any cases where no heading is needed.

--- apps/web/app/home/_components/about-us/AboutUs.tsx
+++ apps/web/app/home/_components/about-us/AboutUs.tsx
@@ -9,13 +9,13 @@ const cx = classNames.bind(Style);
 
 export default function AboutUs() {
   return (
-    <section className={cx('about-us')}>
-      <Container className={cx('wrap')} applyMargin="left-right" margin="m">
+    <Container className={cx('about-us')} margin="none">
+      <Container applyMargin="left-right" margin="m">
         <TextFillAnimation text={`We're a trip planner`} />
         <TextFillAnimation text={`Enhanced with AI.`} endColor={COLORS[50]} />
         <TextFillAnimation text={`We help plan your trips`} />
         <TextFillAnimation text={`with the power of AI ✨`} endColor={COLORS[50]} />
       </Container>
-    </section>
+    </Container>
   );
 }
