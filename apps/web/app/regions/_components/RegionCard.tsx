e37bcb1e9c8c5e496b2ee24520365526b502185b
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2025-03-19 18:21:02 +0900

    fix: Error: Element div not allowed as child of element ul in this context.

--- apps/web/app/regions/_components/RegionCard.tsx
+++ apps/web/app/regions/_components/RegionCard.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { Card } from '@tripie-pyotato/design-system';
+import { Card, List } from '@tripie-pyotato/design-system';
 import useImgAlt from 'hooks/useImgAlt';
 import { RegionArticleInfo } from 'models/Article';
 import { useRouter } from 'next/navigation';
@@ -11,17 +11,19 @@ const RegionCard = ({ article }: { article: RegionArticleInfo }) => {
   const { alt } = useImgAlt({ imgUrl: article.source.image.sizes.full.url });
 
   return (
-    <Card.WithCoverImage
-      src={article.source.image.sizes.small_square.url}
-      alt={alt}
-      sourceUrl={article.source.image?.sourceUrl}
-      title={article?.source?.title}
-      summary={article?.source?.summary}
-      imageSize="card"
-      withBorder={false}
-      blurDataURL={article.source.image.blurData?.data}
-      onClick={() => navigate.push(`/posts/${article.source.regionId}/articles/${article.id}`)}
-    />
+    <List.Item>
+      <Card.WithCoverImage
+        src={article.source.image.sizes.small_square.url}
+        alt={alt}
+        sourceUrl={article.source.image?.sourceUrl}
+        title={article?.source?.title}
+        summary={article?.source?.summary}
+        imageSize="card"
+        withBorder={false}
+        blurDataURL={article.source.image.blurData?.data}
+        onClick={() => navigate.push(`/posts/${article.source.regionId}/articles/${article.id}`)}
+      />
+    </List.Item>
   );
 };
 
