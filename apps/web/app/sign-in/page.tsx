c7ea392fc39e442e7d3611db0c1b1b4f3929f1e6
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2025-04-01 16:40:23 +0900

    chore: remove unused Nav comp and Container wrap

--- apps/web/app/sign-in/page.tsx
+++ apps/web/app/sign-in/page.tsx
@@ -1,4 +1,3 @@
-import { Container } from '@tripie-pyotato/design-system';
 import { AuthError } from 'next-auth';
 import { redirect } from 'next/navigation';
 import { classNames } from 'wrapper';
@@ -14,38 +13,34 @@ const cx = classNames.bind(Style);
 //   const { callbackUrl } = await props.searchParams;
 export default async function SignIn(props: Readonly<{ searchParams: { callbackUrl?: string } }>) {
   const { callbackUrl } = props.searchParams;
-  return (
-    <Container applyMargin="top-bottom">
-      {Object.values(providerMap).map(provider => (
-        <form
-          key={'item' + provider.name}
-          className={cx('submit')}
-          action={async () => {
-            'use server';
-            try {
-              await signIn(provider.id, {
-                redirectTo: callbackUrl ?? '',
-              });
-            } catch (error) {
-              // Signin can fail for a number of reasons, such as the user
-              // not existing, or the user not having the correct role.
-              // In some cases, you may want to redirect to a custom error
-              if (error instanceof AuthError) {
-                // return redirect(`${SIGNIN_ERROR_URL}?error=${error.type}`);
-                return redirect(`/error?error=${(error as AuthError).type}`);
-              }
+  return Object.values(providerMap).map(provider => (
+    <form
+      key={'item' + provider.name}
+      className={cx('submit')}
+      action={async () => {
+        'use server';
+        try {
+          await signIn(provider.id, {
+            redirectTo: callbackUrl ?? '',
+          });
+        } catch (error) {
+          // Signin can fail for a number of reasons, such as the user
+          // not existing, or the user not having the correct role.
+          // In some cases, you may want to redirect to a custom error
+          if (error instanceof AuthError) {
+            // return redirect(`${SIGNIN_ERROR_URL}?error=${error.type}`);
+            return redirect(`/error?error=${(error as AuthError).type}`);
+          }
 
-              // Otherwise if a redirects happens Next.js can handle it
-              // so you can just re-thrown the error and let Next.js handle it.
-              // Docs:
-              // https://nextjs.org/docs/app/api-reference/functions/redirect#server-component
-              throw error;
-            }
-          }}
-        >
-          <OAuthButton provider={provider} />
-        </form>
-      ))}
-    </Container>
-  );
+          // Otherwise if a redirects happens Next.js can handle it
+          // so you can just re-thrown the error and let Next.js handle it.
+          // Docs:
+          // https://nextjs.org/docs/app/api-reference/functions/redirect#server-component
+          throw error;
+        }
+      }}
+    >
+      <OAuthButton provider={provider} />
+    </form>
+  ));
 }
