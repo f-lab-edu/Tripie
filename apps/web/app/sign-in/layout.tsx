c8b849313963547b53146e06225b9e20eaa5a5cf
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2025-04-01 16:40:58 +0900

    chore: remove unused Nav comp and Container wrap

--- apps/web/app/sign-in/layout.tsx
+++ apps/web/app/sign-in/layout.tsx
@@ -12,9 +12,7 @@ export default function Layout({
   children: ReactNode;
 }>) {
   return (
-    // <Container margin="none" className={cx('background')}>
     <Container className={cx('background', 'header')} margin="none">
-      {/* <Nav /> */}
       <Container className={cx('gradient-bottom')} margin="none" />
       <Container margin="none" className={cx('login-wrap')}>
         <Card className={cx('login-card')}>
@@ -27,11 +25,10 @@ export default function Layout({
             모든 서비스를 이용하기 위해 {'\n'}로그인을 해주세요.
           </Container>
           <Divider />
-          {children}
+          <Container applyMargin="top-bottom">{children}</Container>
         </Card>
       </Container>
       <ParticleField />
     </Container>
-    // </Container>
   );
 }
