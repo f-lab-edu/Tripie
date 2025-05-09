fec0699ff295dc6235d4b1809714927ca3853a36
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2025-05-09 14:42:58 +0900

    fix: card padding for plan list cards

--- apps/web/app/home/_components/plan/PlanList.tsx
+++ apps/web/app/home/_components/plan/PlanList.tsx
@@ -6,46 +6,10 @@ import PLANS from 'constants/plans';
 
 export default function PlanList() {
   return (
-    <Stack
-      // display="grid"
-      margin="l"
-      applyMargin="bottom"
-      flexWrapOn="wrap-lg"
-      gap="l"
-    >
+    <Stack margin="l" applyMargin="bottom" flexWrapOn="wrap-lg" gap="l">
       {Object.keys(PLANS).map(key => (
-        // <Card key={PLANS[key].label}>
-        // <Container justifyContent="start" margin="none">
-        //   {PLANS[key].label}
-        // </Container>
-        // <Container justifyContent="start" applyMargin="bottom" margin="xsm">
-        //   <Headings.H3>
-        //     <Text.Accented>{PLANS[key].price}</Text.Accented>
-        //   </Headings.H3>
-        // </Container>
-        //   <Container applyMargin="bottom" justifyContent="start">
-        //     Per month
-        //   </Container>
-        //   <Divider />
-        // <List view={'column'} gap={'default'}>
-        //   {PLANS[key].items.map(({ label, description, icon }) => (
-        //     <List.Item gap={'default'} justifyContent={'flex-start'} alignItems={'center'} key={label}>
-        //       {icon === 'X' ? <Icon.X /> : <Icon.Check />}
-        //       <Text crossOut={icon === 'X'}>{description}</Text>
-        //     </List.Item>
-        //   ))}
-        // </List>
-        // <AnimatedButton
-        //   isFullSize={true}
-        //   withBorder={true}
-        //   withMinWidth={true}
-        //   onClick={() => alert(`chose ${PLANS[key].label}`)}
-        // >
-        //   Get started with {PLANS[key].label}
-        // </AnimatedButton>
-        // </Card>
-        <TripieCard key={PLANS[key].label} margin="none" sizes="full">
-          <TripieCard.Header applyMargin={'top-left-right'}>
+        <TripieCard key={PLANS[key].label} margin="none" sizes="full" padding="m">
+          <TripieCard.Header margin="none">
             <Stack direction="column" margin="none">
               {PLANS[key].label}
               <Text size="h3">
@@ -53,8 +17,10 @@ export default function PlanList() {
               </Text>
             </Stack>
           </TripieCard.Header>
-          <TripieCard.Content>Per month</TripieCard.Content>
-          <TripieCard.Divider />
+          <TripieCard.Content applyMargin="bottom" margin="sm">
+            Per month
+          </TripieCard.Content>
+          <TripieCard.Divider margin="none" />
           <TripieCard.Content>
             <List view={'column'} gap={'default'}>
               {PLANS[key].items.map(({ label, description, icon }) => (
