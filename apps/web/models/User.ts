0e4d59fcebb849160ca0538c9d2ea101b72163cb
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2026-01-19 16:31:31 +0900

    feat: add ip & id for user model

--- apps/web/models/User.ts
+++ apps/web/models/User.ts
@@ -6,11 +6,20 @@ export type Token = {
   iat: number;
   exp: number;
   jti: string;
+  ip?: string;
+  id?: string;
 };
 
 export type User = {
   session: {
-    user: { name: Token['name']; email?: Token['email']; image?: string; id?: string; picture?: Token['picture'] };
+    user: {
+      name: Token['name'];
+      email?: Token['email'];
+      image?: string;
+      id?: string;
+      picture?: Token['picture'];
+      ip?: string;
+    };
     expires: string;
     token?: Token;
   };
