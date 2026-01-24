4ba91fbb400dfac4958b743e72b9043f5111b73c
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2026-01-24 14:53:45 +0900

    chore: change export format

--- packages/design-system/src/wrappers/react-calendar.ts
+++ packages/design-system/src/wrappers/react-calendar.ts
@@ -1,4 +1,3 @@
 'use client';
-import { CalendarProps, Calendar as ReactCalendar } from 'react-calendar';
-import { LooseValue, Range, Value } from 'react-calendar/dist/esm/shared/types.js';
-export { ReactCalendar, type CalendarProps, type LooseValue, type Range, type Value };
+export { Calendar as ReactCalendar, type CalendarProps } from 'react-calendar';
+export { type LooseValue, type Range, type Value } from 'react-calendar/dist/esm/shared/types.js';
