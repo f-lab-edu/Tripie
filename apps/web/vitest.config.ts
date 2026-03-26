621a8437115d53a88a233ccf76c5fb1577a977c8
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2026-03-26 14:54:21 +0900

    chore: update import

--- apps/web/vitest.config.ts
+++ apps/web/vitest.config.ts
@@ -1,3 +1,27 @@
-import { uiConfig } from '@tripie-pyotato/vitest-config/ui';
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
+import { defineConfig } from 'vitest/config';
 
-export default uiConfig;
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+
+export default defineConfig({
+  test: {
+    environment: 'jsdom',
+    coverage: {
+      provider: 'istanbul',
+    },
+  },
+  resolve: {
+    alias: {
+      utils: path.resolve(__dirname, 'utils'),
+      models: path.resolve(__dirname, 'models'),
+      constants: path.resolve(__dirname, 'constants'),
+      hooks: path.resolve(__dirname, 'hooks'),
+      app: path.resolve(__dirname, 'app'),
+      shared: path.resolve(__dirname, 'shared'),
+    },
+  },
+  css: {
+    postcss: { plugins: [] },
+  },
+});
