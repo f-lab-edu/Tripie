81cc552eee5765091576ba885d5bb209aac0201f
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2026-03-30 18:30:52 +0900

    fix: token status

--- apps/web/shared/components/Nav/TokenStatus.tsx
+++ apps/web/shared/components/Nav/TokenStatus.tsx
@@ -3,13 +3,33 @@
 import AnimatedText from '@tripie-pyotato/design-system/@components/AnimatedText';
 import Text from '@tripie-pyotato/design-system/@core/Text';
 
-import { Divider, Link } from '@tripie-pyotato/design-system/@components';
+import ROUTE from '@/constants/routes';
+import { Divider } from '@tripie-pyotato/design-system/@components';
+import UnstyledLink from '@tripie-pyotato/design-system/@components/Link';
 import { Container } from '@tripie-pyotato/design-system/@core';
+import { COLORS } from '@tripie-pyotato/design-system/shared';
 import useChatToken from 'hooks/useChatToken';
+import { usePathname } from 'next/navigation';
+import { useMemo } from 'react';
 import TripieIcon from '../TripieIcon/TripieIcon';
 
 const TokenStatus = () => {
   const { remainingToken, isAdmin, usedGptToken, status } = useChatToken();
+  const pathName = usePathname();
+  const fontColor = useMemo(() => (pathName.startsWith(ROUTE.TRIP_PLANNER.href) ? 400 : 50), [pathName]);
+
+  if (status === 'unauthenticated') {
+    return (
+      <Container margin="none">
+        <UnstyledLink href={'/sign-in'}>
+          <Text size="tiny" color={COLORS[fontColor]}>
+            로그인 후 추천받기
+          </Text>
+        </UnstyledLink>
+        <Divider />
+      </Container>
+    );
+  }
 
   if (status === 'loading') {
     return (
@@ -18,16 +38,7 @@ const TokenStatus = () => {
       </Text>
     );
   }
-  if (status === 'unauthenticated') {
-    return (
-      <Container margin="none">
-        <Link href={'/sign-in'}>
-          <Text size="tiny">로그인 후 추천받기</Text>
-        </Link>
-        <Divider />
-      </Container>
-    );
-  }
+
   if (isAdmin && usedGptToken != null) {
     return (
       <Text size="tiny" margin="sm" applyMargin="all">
@@ -37,6 +48,13 @@ const TokenStatus = () => {
   }
 
   if (Number.isInteger(remainingToken)) {
+    if (remainingToken === 0) {
+      return (
+        <Text size="tiny" margin="sm" applyMargin="all">
+          토큰 소진!
+        </Text>
+      );
+    }
     return (
       <Text size="tiny" margin="sm" applyMargin="all">
         {remainingToken} 토큰
